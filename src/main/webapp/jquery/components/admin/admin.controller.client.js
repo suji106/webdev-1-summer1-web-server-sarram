f464fbee27bfb867affb298e79be81f8f331818f
Sujith Srivardhan Arram <suji.srivardhan@gmail.com>
2018-05-13 17:00:10 -0400

    Adding the css and controller filr for the admin page

---
+++ src/main/webapp/jquery/components/admin/admin.controller.client.js
@@ -0,0 +1,127 @@
+(function () {
+	var $usernameFld, $passwordFld;
+	var $removeBtn, $editBtn, $createBtn;
+	var $firstNameFld, $lastNameFld;
+	var $userRowTemplate, $tbody;
+	$(main);
+	
+	var userService = new UserServiceClient()
+	
+	function main() {		
+		tbody = $('tbody');
+		$userRowTemplate = $('.xxx');
+		$('#createUser').click(createUser);
+		$('#updateUser').click(updateUser);
+		$('#searchUser').click(searchUser);
+	
+		findAllUsers();
+	}
+	
+	function searchUser(event) {
+		console.log('deleteUser');
+
+		var searchBtn = $(event.currentTarget);
+        var userId = searchBtn
+            .parent()
+            .parent()
+            .attr('id');
+        console.log(userId);
+
+        userService.searchUser(342).then(renderUsers);
+    }
+	
+	function deleteUser(event) {
+		console.log('deleteUser');
+
+		var deleteBtn = $(event.currentTarget);
+        var userIdBody = deleteBtn
+            .parent()
+            .parent()
+            .parent();
+        
+        console.log(userIdBody.html());
+        
+        var userId = userIdBody.attr('id');
+
+        userService.deleteUser(userId).then(renderUsers);
+    }
+	
+	function editUser(event) {
+		console.log('deleteUser');
+
+		var editBtn = $(event.currentTarget);
+        var userIdBody = editBtn
+            .parent()
+            .parent()
+            .parent();
+        
+        console.log(userIdBody.html());
+        
+        var userId = userIdBody.attr('id');
+
+        userService.editUser(userId).then(renderUsers);
+    }
+	
+	function updateUser() {
+		console.log('updateUser');
+
+		var username = $('#usernameFld').val();
+		var password = $('#passwordFld').val();
+		var firstName = $('#firstNameFld').val();
+		var lastName = $('#lastNameFld').val();
+
+		var user = {
+				username: username,
+				password: password,
+				firstName: firstName,
+				lastName: lastName
+		};
+
+		userService
+		.updateUser(342, user)
+		.then(findAllUsers);
+	}
+	
+	function createUser() {
+		console.log('createUser');
+
+		var username = $('#usernameFld').val();
+		var password = $('#passwordFld').val();
+		var firstName = $('#firstNameFld').val();
+		var lastName = $('#lastNameFld').val();
+
+		var user = {
+				username: username,
+				password: password,
+				firstName: firstName,
+				lastName: lastName
+		};
+
+		userService
+		.createUser(user, function() {
+			  alert("New user created");
+		})
+		.then(findAllUsers);
+	}
+
+	function findAllUsers() {
+		console.log('findAllUsersController');
+		userService.findAllUsers().then(renderUsers);
+	}
+	
+	function renderUsers(users) {
+		console.log("Rendering users");
+        tbody.empty();
+        for(var i=0; i<users.length; i++) {
+            var user = users[i];
+            var clone = $userRowTemplate.clone(); 
+            clone.attr('id', user.id);
+            clone.find('.wbdv-username').html(user.username);
+            clone.find('.wbdv-first-name').html(user.firstName);
+            clone.find('.wbdv-last-name').html(user.lastName);
+            clone.find('.wbdv-remove').click(deleteUser);
+            // clone.find('.wbdv-edit').click(editUser);
+            tbody.append(clone);
+        }
+    }
+})();
